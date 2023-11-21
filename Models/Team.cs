b18d7ee7e19b49814e27db937459c742df327817
Iamneo <techadmin@iamneo.ai>
2023-11-21 12:05:31 +0000

    1

---
+++ Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+ 
+namespace dotnetapp.Models
+{
+        public class Team
+        {
+            [Key]
+            public int TeamId {get; set;}
+            public string TeamName {get; set;}
+            public ICollection<Player> ?Player {get; set;}
+        }
+} 
